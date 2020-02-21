8f04e97c610af5173d92dbebe9f7fe262c7247ae
_Checkmate_ <checkm8ey@protonmail.com>
2020-02-21 07:32:44 +0000

    dust limits

--- btconchain/btc_dust_limit.py
+++ btconchain/btc_dust_limit.py
@@ -1,42 +1,49 @@
 #Calculate the dust limit and estimate future value
 from checkonchain.btconchain.btc_add_metrics import *
 from checkonchain.general.standard_charts import *
+from checkonchain.general.regression_analysis import *
 
 #Set Constants
-blk_max = 210000*6 #max block height to calculate up to
-dustlim = 172 #sats
-sats = 1e8 #sats per BTC
+blk_max = 210000*6  #max block height to calculate up to
+dustlim = 172       #bytes
+max_fee = 0.01      #Assume 1% of transaction cost is max viable fee
+sats = 1e8          #sats per BTC
 
 #Pull Coinmetrics data
 BTC_coin = btc_add_metrics().btc_coin()
+blk_cur = BTC_coin['blk'].max() #Current block
 BTC_coin = BTC_coin.loc[:,[
     'date','blk',
+    'CapMrktCurUSD',
     'PriceUSD','PriceRealised','SplyCur',
     'BlkCnt','BlkSizeByte','BlkSizeMeanByte	',
     'FeeMeanNtv','FeeMeanUSD',
     'FeeMedNtv','FeeMedUSD',
     'FeeTotNtv','FeeTotUSD',
-    'TxCnt','TxTfr'
+    'TxCnt','TxTfrCnt'
     ]]
 
 
 #Calculate Actual Fee performance
+#USD cost of a dust transaction
 BTC_coin['DustPrice']=dustlim/sats * BTC_coin['PriceUSD']
-BTC_coin['FeeSatsMean']=BTC_coin['FeeMeanUSD']/BTC_coin['PriceUSD']
-BTC_coin['FeeSatsMed']=BTC_coin['FeeMedUSD']/BTC_coin['PriceUSD']
+#Minimum viable USD transaction assuming max_fee rate
+BTC_coin['MinViableTxUSD'] = BTC_coin['DustPrice'] / max_fee
+#Calculate the average bytes per Tx (TxCnt) and TxTfr (TxTfr)
+#Note assume TxTfr is most representative as all fee paying Tx are useful
 BTC_coin['TxCntSizeByte']=BTC_coin['BlkSizeByte']/BTC_coin['TxCnt']
-BTC_coin['TxTfrSizeByte']=BTC_coin['BlkSizeByte']/BTC_coin['TxTfr']
-BTC_coin['DustSizeByte']=dustlim
-BTC_coin['DustSizeByte']=dustlim
-BTC_coin['FeeTxCnt']=BTC_coin['TxCntSizeByte']/sats*BTC_coin['PriceUSD']
-BTC_coin['FeeTxTfr']=BTC_coin['TxTfrSizeByte']/sats*BTC_coin['PriceUSD']
+BTC_coin['TxTfrSizeByte']=BTC_coin['BlkSizeByte']/BTC_coin['TxTfrCnt']
+BTC_coin['DustSizeByte'] = dustlim #dust limit in bytes
+#Calculate mean and median Fees paid priced in Sats/byte
+#Mean Fee(BTC) * Sats = Mean Fee(sats) / Mean Bytes per Tx = Mean sats/byte
+BTC_coin['FeeSatsByteMean']=BTC_coin['FeeMeanNtv'] * sats / BTC_coin['TxTfrSizeByte']
+BTC_coin['FeeSatsByteMed']=BTC_coin['FeeMedNtv'] * sats / BTC_coin['TxTfrSizeByte']
 
-BTC_coin['FeeSatsMean']
-BTC_coin['FeeMeanNtv']
 
-#Calculate supply function
+#Calculate Supply Function
 BTC_sply = btc_add_metrics().btc_sply_curtailed(blk_max)
-BTC_sply['S2F_1sats_byte'] = BTC_sply['PricePlanBmodel'] * dustlim/sats
+#Calculate USD Price per Transaction (assume sats per byte)
+BTC_sply['S2F_1sats_byte'] = BTC_sply['PricePlanBmodel'] * dustlim / sats
 BTC_sply['S2F_2sats_byte'] = BTC_sply['S2F_1sats_byte'] * 2
 BTC_sply['S2F_10sats_byte'] = BTC_sply['S2F_1sats_byte'] * 10
 BTC_sply['S2F_30sats_byte'] = BTC_sply['S2F_1sats_byte'] * 30
@@ -44,17 +51,13 @@ BTC_sply['S2F_100sats_byte'] = BTC_sply['S2F_1sats_byte'] * 100
 BTC_sply['S2F_200sats_byte'] = BTC_sply['S2F_1sats_byte'] * 200
 
 
+"""
+#############################################################################
+                    BTC FEE PRICES USD
+#############################################################################
+Shows the growth of retail user fees assuming Plan B S2F model
+"""
 
-"""%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-CREATE PLOT 01
-DUST COMPARED TO FEE LIMITS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"""
-
-#Create Plot against block height
-# Y-axis 1 = Fee (USD)
-# Y-axs 2 = BTCUSD Price
-
-# Create Input Dataset
 loop_data=[[0,1,2,3,4,5,6,7,8],[9]]
 x_data = [
     BTC_sply['blk'],BTC_sply['blk'],BTC_sply['blk'],
@@ -138,101 +141,160 @@ BTC_sply['CapPlanBmodel'].where(BTC_sply['CapPlanBmodel'] <= maxVal, maxVal)
 
 
 
-"""%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-CREATE PLOT 02 
-FEE RATE OVER TIME IN SATS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"""
-
-fig_02 = make_subplots(specs=[[{"secondary_y": True}]])
-fig_02.update_layout(template="plotly_dark",title="Fee Rates")
-fig_02.add_trace(go.Scatter(
-    x=BTC_coin['blk'], y=BTC_coin['FeeMeanNtv'].rolling(14).mean()*sats/dustlim,
-    name="Mean Fee Rate",line=dict(width=2,color='rgb(102, 255, 153)')),
-    secondary_y=False)
-fig_02.add_trace(go.Scatter(
-    x=BTC_coin['blk'], y=BTC_coin['FeeMedNtv'].rolling(14).mean()*sats/dustlim,
-    name="Median Fee Rate",line=dict(width=2,color='rgb(255, 204, 102)')),
-    secondary_y=False)
-
-fig_02.add_trace(go.Scatter(
-    x=BTC_coin['blk'], y=BTC_coin['PriceUSD'],
-    name="BTCUSD Price",line=dict(width=2,color='rgb(255, 255, 255)')),
-    secondary_y=True)
-fig_02.add_trace(go.Scatter(
-    x=BTC_sply['blk'], y=BTC_sply['PricePlanBmodel'],
-    name="S2F Model Price",line=dict(width=2,dash='dash',color='rgb(102, 153, 255)')),
-    secondary_y=True)
-
-fig_02.update_xaxes(
-    title_text="<b>Block Height</b>",
-    type="linear",
-    range=[70000,blk_max]
+"""
+#############################################################################
+                    BYTES PER TX AND SATS/BYTE
+#############################################################################
+"""
+loop_data=[[0,1,2,3,4],[5,6]]
+x_data = [
+    BTC_coin['blk'],
+    BTC_coin['blk'],
+    BTC_coin['blk'],
+    BTC_coin['blk'],
+    BTC_coin['blk'],
+    BTC_coin['blk'],
+    BTC_coin['blk'],
+    ]
+y_data = [
+    BTC_coin['TxCntSizeByte'],
+    BTC_coin['TxCntSizeByte'].rolling(90).mean(),
+    BTC_coin['TxTfrSizeByte'],
+    BTC_coin['TxTfrSizeByte'].rolling(90).mean(),
+    BTC_coin['DustSizeByte'],
+    BTC_coin['FeeSatsByteMean'].rolling(14).mean(),
+    BTC_coin['FeeSatsByteMed'].rolling(14).mean(),
+]
+name_data = [
+    'Bytes per TxCnt',
+    'Bytes per TxCnt (90 DMA)',
+    'Bytes per TxTfr',
+    'Bytes per TxTfr (90 DMA)',
+    'Dust Limit',
+    'Mean Sats/byte',
+    'Median Sats/byte',
+]
+width_data      = [
+    1,3,1,3,3,
+    1,1
+    ]
+opacity_data    = [
+    0.5,1,0.5,1,1,
+    0.5,0.5
+    ]
+dash_data = [
+    'solid','dash','solid','dash','dash',
+    'solid','solid',
+]
+color_data = [
+    'rgb(102, 255, 153)',   #Turquoise Green
+    'rgb(1, 255, 116)',     #Green
+    'rgb(102, 204, 255)',   #L.Blue
+    'rgb(20, 169, 233)',       #D.Blue
+    'rgb(255, 80, 80)',      #White
+    'rgb(254, 215, 140)',   #Matte Yellow
+    'rgb(255, 102, 0)' ,    #Burnt Orange
+]
+legend_data = [
+    False,True,False,True,True,
+    True,True
+    ]
+title_data = [
+    'Bitcoin Transaction Byte Size',
+    '<b>Block Height</b>',
+    '<b>Transaction Size (bytes)</b>',
+    '<b>Sats per byte</b>']
+type_data = ['linear','log','log']
+range_data = [[0,650000],[-6,4],[0,3]]
+autorange_data = [False,True,False]
+type_data = ['linear','log','log']#
+fig = check_standard_charts().subplot_lines_doubleaxis(
+    title_data, range_data ,autorange_data ,type_data,
+    loop_data,x_data,y_data,name_data,color_data,
+    dash_data,width_data,opacity_data,legend_data
     )
-fig_02.update_yaxes(
-    title_text="<b>Fee Value (sats)</b>",
-    type="log",
-    secondary_y=False,
-    range=[0,4]
-    )
-fig_02.update_yaxes(
-    title_text="<b>BTCUSD Price</b>",
-    tickformat = '$0:.2f',
-    type="log",
-    secondary_y=True,
-    range=[-2,8],
-    color='rgb(102, 102, 153)'
-    )
-fig_02.show()
+fig.show()
 
 
 
-"""%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-CREATE PLOT 03
-AVERAGE TRANSACTION SIZE
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"""
-fig_03 = make_subplots(specs=[[{"secondary_y": False}]])
-fig_03.update_layout(template="plotly_dark",title="Average Transaction Size (bytes)")
-#Create plot for average and median transaction size in bytes
-fig_03.add_trace(go.Scatter(
-    x=BTC_coin['blk'], y=BTC_coin['TxCntSizeByte'].rolling(14).mean(),
-    name="Average Transaction Size",line=dict(width=1,color='rgb(255, 204, 102)')),
-    secondary_y=False)
-fig_03.update_xaxes(
-    title_text="<b>Block Height</b>",
-    type="linear",
-    range=[0,blk_max]
-    )
-fig_03.update_yaxes(
-    title_text="<b>Average Transaction Size (byte)</b>",
-    type="linear",
-    color='rgb(255, 204, 102)',
-    secondary_y=False,
-    range=[100,2000]
-    )
-fig_03.show()
-    
+
+"""
+#############################################################################
+                    LINEAR REGRESSION - TxTfr vs MC
+#############################################################################
+"""
+#Performan Linear Regression between x-Market Cap and y-TxTfr
+# Intention is to estimate growth of transaction base vs Market Value
+#Concept of Veblen good, more value = more demand
+BTC_regr = BTC_coin[['CapMrktCurUSD','TxTfrCnt','blk']]
+#Fill MCap forwards from early pricing data and then eliminate zeros (log-log regression)
+BTC_regr['CapMrktCurUSD'] = BTC_coin['CapMrktCurUSD'].fillna(method='ffill')
+BTC_regr['CapMrktCurUSD'] = BTC_regr['CapMrktCurUSD'].fillna(value=0)
+BTC_regr = BTC_regr[BTC_regr['CapMrktCurUSD']>0]
+BTC_regr = BTC_regr[BTC_regr['TxTfrCnt']>0]
+#Run Regression Model and extract parameters
+model = regression_analysis().ln_regression(BTC_regr,'CapMrktCurUSD','TxTfrCnt','blk')
+intercept = float(model['model_params']['intercept'])
+coefficient = float(model['model_params']['coefficient'])
+#Apply regression model assuming Plan Bs MCap S2f model
+BTC_sply_fwd = BTC_sply#[BTC_sply['blk']>blk_cur]
+BTC_sply_fwd = BTC_sply_fwd[['blk','Sply_ideal','PricePlanBmodel','CapPlanBmodel']]
 
 
-"""%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-CREATE PLOT 04
-BLOCKCHAIN SIZE
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"""
-fig_04 = make_subplots(specs=[[{"secondary_y": False}]])
-fig_04.update_layout(template="plotly_dark")
-#Create plot for average and median transaction size in bytes
-fig_04.add_trace(go.Scatter(
-    x=BTC_coin['blk'], y=BTC_coin['BlkSizeByte'].cumsum()/1048576,
-    name="Blockchain Size (MB)",
-    line=dict(color='rgb(255, 204, 102)',width=3)),
-    secondary_y=False)
-fig_04.update_xaxes(
-    title_text="<b>Block Height</b>",
-    type="linear",
-    range=[0,600000]
+BTC_sply_fwd['TxTfrCnt'] = np.exp(intercept + coefficient * np.log(BTC_sply_fwd['CapPlanBmodel']))
+
+
+
+
+loop_data=[[0,1,],[]]
+x_data = [
+    BTC_coin['CapMrktCurUSD'],
+    BTC_sply_fwd['CapPlanBmodel'],
+    ]
+y_data = [
+    BTC_coin['TxTfrCnt'],
+    BTC_sply_fwd['TxTfrCnt'],
+
+]
+name_data = [
+    'Actual M.Cap vs TxTfrCnt',
+    'Theoretical M.Cap vs TxTfrCnt',
+]
+width_data      = [
+    3,3,
+    ]
+opacity_data    = [
+    1,1
+    ]
+dash_data = [
+    'solid','dash',
+]
+color_data = [
+    'rgb(102, 255, 153)',   #Turquoise Green
+    'rgb(255, 102, 0)' ,    #Burnt Orange
+    'rgb(1, 255, 116)',     #Green
+    'rgb(102, 204, 255)',   #L.Blue
+    'rgb(20, 169, 233)',     #D.Blue
+    'rgb(255, 80, 80)',      #White
+    'rgb(254, 215, 140)',
+    'rgb(255, 102, 0)' ,
+]
+legend_data = [
+    True,True,False,True,True,
+    True,True
+    ]
+title_data = [
+    'A Veblen Good? - Market Cap to Transaction Demand (log-log)',
+    '<b>ln(Market Cap)</b>',
+    '<b>ln(Transaction Count)</b>',
+    '<b>Sats per byte</b>']
+type_data = ['linear','log','log']
+range_data = [[4,12],[1,7],[0,3]]
+autorange_data = [False,False,True]
+type_data = ['log','log','log']#
+fig = check_standard_charts().subplot_lines_singleaxis(
+    title_data, range_data ,autorange_data ,type_data,
+    loop_data,x_data,y_data,name_data,color_data,
+    dash_data,width_data,opacity_data,legend_data
     )
-fig_04.update_yaxes(
-    title_text="<b>Blockchain Size (MB)</b>",
-    type="log",
-    secondary_y=False
-    )
-fig_04.show()
+fig.show()
---
+++ research_articles/btc_dust_limit/btc_dust_limit.md
@@ -0,0 +1,21 @@
+# Forecasting the Bitcoin Fee Market
+
+
+
+## Concepts
+
+- Veblen good, as price moves up so does demand (links transactions to Market Cap), assume linear regression
+- Market Cap has some natural ceiling height (80T = USD, 100T = Plan B Model, 500T = hyperbitcoinisation)
+- Fee pressure builds in USD terms which also constrains demand (relationship between fees and demand?). Assume some maximum acceptable fee ratio to spend (i.e. fee of 1% = 1$ on $100 spend, likely the absolute maximum
+- Balance between ff-chain solutions like LN (Assume that the fee limit is a driver for this. Removes transaction fee income for moiners, miners need to sell more, price goes down, cycle repeats))
+
+
+## Points of Research and Assumptions
+- Ceiling market value (total cash, total SoV)
+- Limits of a
+
+![btc_tx_size_bytes](images/btc_tx_size_bytes.png)
+
+
+![btc_dust_limit_uncapped](images/btc_dust_limit_uncapped.png)
+
